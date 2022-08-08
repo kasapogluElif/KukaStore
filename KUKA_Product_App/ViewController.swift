d65c7ab4f2b2f6bae3d6663125b59ab3b34fe7ae
Elif Kasapoglu <kasapoglu.ek@gmail.com>
2022-08-08 18:32:05 +0300

    Initial Commit

---
+++ KUKA_Product_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KUKA_Product_App
+//
+//  Created by Elif Kasapoglu on 8.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
